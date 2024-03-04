d36bc41b90c4be742cf7e75acb5faea0f4f19bdd
Jeremy.Perez_Flores <jeremy.perez_flores@dachstiftung-diakonie.de>
2024-03-04 13:15:50 +0100

    refactoring

--- Server-Projekte/Wiki/base/base.js
+++ Server-Projekte/Wiki/base/base.js
@@ -1,4 +1,10 @@
 document.write(`
+
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 <nav class="navbar">
 <ul class="nav-list" id="topic_list">
     <li><a href="index.html">Home</a></li>
