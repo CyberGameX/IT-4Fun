293f065d3fb96d4c103a53342ecf97ffcb912fcb
Jeremy.Perez_Flores <jeremy.perez_flores@dachstiftung-diakonie.de>
2024-03-04 12:45:57 +0100

    added base

---
+++ Server-Projekte/Wiki/base/base.js
@@ -0,0 +1,17 @@
+document.write(`
+<nav class="navbar" >
+<ul class="nav-list" id="topic_list">
+    <li><a href="netzwerk.html">Netzwerk-Technik</a></li>
+    <li><a href="programmieren.html">Programmieren</a></li>
+</ul>
+<!-- "return false;" prevents the side from reloading  -->
+<form onsubmit="searchFunction('search_input', 'topic_list'); return false;"> 
+    <label>
+        <input type="text" placeholder="Suche" id="search_input">
+    </label>
+    <button type="submit">Suchen</button>
+</form>
+</nav>
+<script type="text/javascript" src="JS/search_funktion.js"></script>
+
+ `);
